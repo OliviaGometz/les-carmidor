415f6dd36c71df07c3482d4fed8a504d976bd2c3
LesCarmidor <irina.veltugio@gmail.com>
2021-08-31 21:21:34 +0200

    feat: test pinch js to zoom on map

--- js/map.js
+++ js/map.js
@@ -65,3 +65,53 @@ const rangeChangeHandler = function() {
 };
 
 zoomRange.addEventListener('input', rangeChangeHandler);
+
+// Zoom with pinch
+
+let mapScale = 1;
+let start = {};
+
+// Calculate distance between two fingers
+const distance = (e) => {
+    return Math.hypot(
+        e.touches[0].pageX - e.touches[1].pageX,
+        e.touches[0].pageY - e.touches[1].pageY
+    );
+};
+
+container.addEventListener('touchstart', (e) => {
+    if (e.touches.length === 2) {
+        e.preventDefault(); // Prevent page scroll
+
+        // Calculate where the fingers have started on the X and Y axis
+        start.x = (e.touches[0].pageX + e.touches[1].pageX) / 2;
+        start.y = (e.touches[0].pageY + e.touches[1].pageY) / 2;
+        start.distance = distance(e);
+    }
+});
+
+container.addEventListener('touchmove', (e) => {
+    if (e.touches.length === 2) {
+        e.preventDefault(); // Prevent page scroll
+
+        // Safari provides e.scale as two fingers move on the screen
+        // For other browsers just calculate the scale manually
+        let scale;
+
+        if (e.scale) {
+            scale = e.scale;
+        } else {
+            const deltaDistance = distance(e);
+            scale = deltaDistance / start.distance;
+        }
+
+        mapScale = Math.min(Math.max(1, scale), 4);
+
+        // Calculate how much the fingers have moved on the X and Y axis
+        const deltaX = (((e.touches[0].pageX + e.touches[1].pageX) / 2) - start.x) * 2; // x2 for accelarated movement
+        const deltaY = (((e.touches[0].pageY + e.touches[1].pageY) / 2) - start.y) * 2; // x2 for accelarated movement
+
+        // Transform the image to make it grow and move with fingers
+        container.style.transform = `translate3d(${deltaX}px, ${deltaY}px, 0) scale(${mapScale})`;
+    }
+});
